fd6fcb10bf8160c426490fb1a19a3def1325e2d7
omrmarketplace <omandtrauch@forbesadvisor.com>
2024-07-24 19:59:32 +0000

    adding submission

--- pages/3_Langchain_Quickstart.py
+++ pages/3_Langchain_Quickstart.py
@@ -42,3 +42,6 @@ def generate_response(input_text):
 with st.form("my_form"):
     text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
     submitted = st.form_submit_button("Submit")
+    
+    if submitted:
+        generate_response(text)
